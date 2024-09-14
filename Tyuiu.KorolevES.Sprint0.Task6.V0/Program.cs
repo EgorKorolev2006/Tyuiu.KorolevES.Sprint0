cfe5ec25b43f3151fa725afb9651482b0a7ad4c2
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-09-14 17:07:45 +0500

    Task6

---
+++ Tyuiu.KorolevES.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KorolevES.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KorolevES.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+
+            Console.ReadKey();
+        }
+    }
+}
