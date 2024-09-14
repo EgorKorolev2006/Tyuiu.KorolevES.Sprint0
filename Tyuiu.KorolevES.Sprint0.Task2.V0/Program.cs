f957f3ebe093918395af2047db566b42f2c8f0a2
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-09-14 14:43:17 +0500

    Task2

---
+++ Tyuiu.KorolevES.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KorolevES.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KorolevES.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Егор"));
+            Console.ReadKey();
+        }
+    }
+}
