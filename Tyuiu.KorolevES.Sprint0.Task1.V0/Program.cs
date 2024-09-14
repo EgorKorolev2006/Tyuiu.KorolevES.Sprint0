968e473f4317be03b9e41b91c67531fbf82e4e21
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-09-14 13:10:39 +0500

    Task1

---
+++ Tyuiu.KorolevES.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KorolevES.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Егор";
+            int age = 18;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age);
+            Console.ReadKey();
+        }
+    }
+}
